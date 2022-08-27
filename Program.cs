1e276da60c49c2ebc147f509c86eeea84009af2b
ChBrus <cbruno.carrillo@gmail.com>
2022-08-26 20:02:39 -0500

    Hola Platzi (Para el curso de C#)

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Help me!!! I don't know how to use C#!!!!");
+namespace helloWorldPlatzi {
+    class Program{
+        public static void Main(string[] args) {
+            Console.WriteLine("Helo Platzi!");
+        }
+    }
+}
